8545b6857c12d75ff4f297b563b0ce10b2e9ad80
brij0 <brijeshthakrar03@gmail.com>
2024-09-20 09:48:25 -0400

    Check

--- Parking lot.py
+++ Parking lot.py
@@ -49,7 +49,7 @@ def send_email_license(image_path, license_plate, color, model, time_of_notice, 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(sender_email, sender_password)
-    server.sendmail(sender_email, receiver_email, msg.as_string())
+    server.sendmail(sender_email, r eceiver_email, msg.as_string())
     server.quit()
 
 
